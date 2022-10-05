f10eea5a788db0b25777389e96c84f27d2e32d7c
Harsh Vyapari <harshv1741@gmail.com>
2022-10-05 06:34:53 +0000

    Adding Fotter

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
     "💖": "sparkling heart",
     "💻": "Laptop",
     "🛑": "Stop Sign",
-    "⌚": "Watch"
+    "⌚": "Watch",
   };
 
   const [userInput, setUserInput] = useState("");
@@ -46,62 +46,59 @@ export default function App() {
         <h2>{userInput}</h2>
       </div>
 
-    <!-- Footer (START) -->
-    <footer class="footer">
-      <div class="footer-container">
-        <!-- Footer Head -->
-        <div class="footer-head">
-          <h2>Contact Me</h2>
-        </div>
+      <footer class="footer">
+        <div class="footer-container">
+          <div class="footer-head">
+            <h2>Contact Me</h2>
+          </div>
 
-        <!-- Footer Icons -->
-        <div class="footer-content">
-          <div class="footer-icon">
-            <a href="https://www.github.com/harshv1741"
-              ><img src="icon/footer-github.svg" alt="Go to my github"
-            /></a>
-          </div>
-          <div class="footer-icon">
-            <a href="https://www.facebook.com/harshv1741"
-              ><img src="icon/footer-facebook.svg" alt="Go to my facebook"
-            /></a>
-          </div>
-          <div class="footer-icon">
-            <a href="https://instagram.com/harshv1741"
-              ><img src="icon/footer-instagram.svg" alt="Go to my instagram"
-            /></a>
-          </div>
-          <div class="footer-icon">
-            <a href="https://www.twitter.com/harshv1741"
-              ><img src="icon/footer-twitter.svg" alt="Go to my twitter"
-            /></a>
-          </div>
-          <div class="footer-icon">
-            <a href="https://www.linkedin.com/in/harshv1741"
-              ><img src="icon/footer-linkedin.svg" alt="Go to my linkedin"
-            /></a>
-          </div>
-          <div class="footer-icon">
-            <a href="mailto:harshv1741@gmail.com"
-              ><img src="icon/footer-gmail.svg" alt="Mail me on Gmail"
-            /></a>
-          </div>
-          <div class="footer-icon">
-            <a href="mailto:harshv1741@outlook.com"
-              ><img src="icon/footer-outlook.svg" alt="Mail me on Outlook"
-            /></a>
+          <div class="footer-content">
+            <div class="footer-icon">
+              <a href="https://www.github.com/harshv1741">
+                <img src="icon/footer-github.svg" alt="Go to my github" />
+              </a>
+            </div>
+            <div class="footer-icon">
+              <a href="https://www.facebook.com/harshv1741">
+                <img src="icon/footer-facebook.svg" alt="Go to my facebook" />
+              </a>
+            </div>
+            <div class="footer-icon">
+              <a href="https://instagram.com/harshv1741">
+                <img src="icon/footer-instagram.svg" alt="Go to my instagram" />
+              </a>
+            </div>
+            <div class="footer-icon">
+              <a href="https://www.twitter.com/harshv1741">
+                <img src="icon/footer-twitter.svg" alt="Go to my twitter" />
+              </a>
+            </div>
+            <div class="footer-icon">
+              <a href="https://www.linkedin.com/in/harshv1741">
+                <img src="icon/footer-linkedin.svg" alt="Go to my linkedin" />
+              </a>
+            </div>
+            <div class="footer-icon">
+              <a href="mailto:harshv1741@gmail.com">
+                <img src="icon/footer-gmail.svg" alt="Mail me on Gmail" />
+              </a>
+            </div>
+            <div class="footer-icon">
+              <a href="mailto:harshv1741@outlook.com">
+                <img src="icon/footer-outlook.svg" alt="Mail me on Outlook" />
+              </a>
+            </div>
+            <br />
+            <h3 id="#page-end">
+              <br />
+              Made with 💖 by Harsh Vyapari <br />
+              <br />
+              Powered by Vercel
+            </h3>
           </div>
           <br />
-          <h3 id="#page-end">
-            <br />
-            Made with 💖 by Harsh Vyapari <br /><br />
-            Powered by Netlify
-          </h3>
         </div>
-        <br />
-      </div>
-    </footer>
-    <!-- Footer (END) -->
+      </footer>
     </div>
   );
 }
